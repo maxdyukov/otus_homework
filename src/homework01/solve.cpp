4739107565e3e052a724a23b737730c0dc3f41da
Maxim Dyukov <maximka.fcsm@gmail.com>
2023-03-20 13:17:43 +0000

    Second test

--- src/homework01/solve.cpp
+++ src/homework01/solve.cpp
@@ -1,8 +1,13 @@
 #include <vector>
+#include <math.h>
 
 std::vector<double> solve(double a, double b, double c) {
     if(b == 0 && a > 0 && c > 0){
       return {};
+    } else if ( b == 0 && a > 0 && c < 0){
+      double root = std::sqrt(std::abs(c));
+      return {root, -1.0 * root};
     }
+
     return {};
 }
--- src/homework01/tests/solve_tests.cpp
+++ src/homework01/tests/solve_tests.cpp
@@ -4,6 +4,14 @@
 std::vector<double> solve(double a, double b, double c);
 
 TEST(SolveTest, NotRootWhenSecondCoefIsZero) { 
-    auto roots = solve(1, 0, 1);
+    auto roots = solve(1.0, 0.0, 1.0);
     EXPECT_TRUE(roots.empty());
+}
+
+TEST(SolveTest, RootsWhenSecondCoefIsZero){
+    auto roots = solve(1.0, 0.0, -1.0);
+    EXPECT_FALSE(roots.empty());
+    EXPECT_EQ(2, roots.size());
+    EXPECT_DOUBLE_EQ(1.0, roots[0]);
+    EXPECT_DOUBLE_EQ(-1.0, roots[1]);
 }
