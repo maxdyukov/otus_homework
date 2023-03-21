e693204776dcb501dfcc5e9809b831a7c1241c39
Maxim Dyukov <maximka.fcsm@gmail.com>
2023-03-21 13:38:54 +0000

    Fifth test

--- src/homework01/solve.cpp
+++ src/homework01/solve.cpp
@@ -2,27 +2,29 @@
 #include <math.h>
 #include <stdexcept>
 
-bool isEqual(double x, double y){
-  return std::fabs(x - y) < std::numeric_limits<double>::min();
-}
+// const double epsilon{std::numeric_limits<double>::min()};
+const double epsilon{0.001};
+
+bool isEqual(double x, double y, double eps) { return std::fabs(x - y) < eps; }
 
 std::vector<double> solve(double a, double b, double c) {
-    if (isEqual(a, 0.0)){
-      throw std::invalid_argument("a is zero");
-    }
-    
-    if(b == 0 && a > 0 && c > 0){
-      return {};
-    } else if ( b == 0 && a > 0 && c < 0){
-      double root = std::sqrt(std::fabs(c));
-      return {root, -1.0 * root};
-    }
+  if (isEqual(a, 0.0, epsilon)) {
+    throw std::invalid_argument("The a is zero");
+  }
 
-    double D = b * b - 4 * a * c;
-    if (D == 0){
-      double x1 = -(b / (2 * a));
-      return {x1, x1};
-    }
+  if(isEqual(b, 0.0, epsilon) && a > 0 && c > 0){
+    return {};
+  } else if (isEqual(b, 0.0, epsilon) && a > 0 && c < 0){
+    double root = std::sqrt(std::fabs(c/a));
+    return {root, -1.0 * root};
+  }
+  double D = b * b - 4 * a * c;
+  if (isEqual(D, 0.0, epsilon)){
+    double x1 = -(b / (2 * a));
+    return {x1, x1};
+  } else {
+      
+  }
 
     return {};
 }
--- src/homework01/tests/solve_tests.cpp
+++ src/homework01/tests/solve_tests.cpp
@@ -2,13 +2,14 @@
 #include <vector>
 
 std::vector<double> solve(double a, double b, double c);
+bool isEqual(double x, double y, double eps);
 
-TEST(SolveTest, NotRootWhenSecondCoefIsZero) { 
+TEST(SolveTest, Not_Root_When_Second_Coef_Is_Zero) { 
     auto roots = solve(1.0, 0.0, 1.0);
     EXPECT_TRUE(roots.empty());
 }
 
-TEST(SolveTest, RootsWhenSecondCoefIsZero){
+TEST(SolveTest, Roots_When_Second_Coef_Is_Zero){
     auto roots = solve(1.0, 0.0, -1.0);
     EXPECT_FALSE(roots.empty());
     EXPECT_EQ(2, roots.size());
@@ -16,7 +17,7 @@ TEST(SolveTest, RootsWhenSecondCoefIsZero){
     EXPECT_DOUBLE_EQ(-1.0, roots[1]);
 }
 
-TEST(SolveTest, OneRoots) { 
+TEST(SolveTest, One_Roots) { 
     auto roots = solve(1.0, 2.0, 1.0);
     EXPECT_DOUBLE_EQ(-1.0, roots[0]);
     EXPECT_DOUBLE_EQ(-1.0, roots[1]);
@@ -24,4 +25,14 @@ TEST(SolveTest, OneRoots) {
 
 TEST(SolveTest, A_Is_Zerro) { 
     EXPECT_THROW(solve(0.0, 2.0, 1.0), std::invalid_argument);
+}
+
+TEST(SolveTest, D_Is_Very_Small) {
+    // a = 1, b = 0.0002, c = 0.0001 
+    double D =  4 * 1 * 0.0001;
+    EXPECT_GE(D, 0.0);
+    EXPECT_TRUE(isEqual(D, 0.0, 0.001));
+
+    auto roots = solve(1, 0.0002, 0.0001);
+    ASSERT_TRUE(roots.empty());
 }
